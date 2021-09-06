2d8435e5333fb25e77e00371cf4b14dcd90a44e3
Lahcene Guenane <80882418+lahcene90@users.noreply.github.com>
2021-09-06 10:32:20 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
+#include <SoftwareSerial.h>
+SoftwareSerial BT(8,7); // For Bleutooth
+//Tx == 8; Rx == 7
 
 // If using the breakout with SPI, define the pins for SPI communication.
 #define PN532_SCK  (2)
@@ -11,35 +14,46 @@
 #define PN532_MISO (5)
 Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
 
-
-
 char * url = "{\"GPS\":\"36.7249122:3.1542043\",\"CONSTITUTION\":\"boom\",\"ND1\":\"035853195\",\"ND2\":\"0\",\"ND3\":\"0\",\"ND4\":\"0\",\"ND5\":\"0\",\"ND6\":\"0\",\"ND7\":\"0\"}";
 uint8_t ndefprefix = NDEF_URIPREFIX_NONE;
 
-void setup(void) {
-  Serial.begin(115200);
-  while (!Serial) delay(10); 
+const int buttonPin = 9; // Botton
 
-  Serial.println("Hello Lahcene");
+
+void setup(void) 
+{
+  
+  pinMode(buttonPin, INPUT);
+  
+  Serial.begin(115200);
+  BT.begin(115200); // For Bleutooth
+  while (!Serial) delay(10); 
 
   nfc.begin();
 
   uint32_t versiondata = nfc.getFirmwareVersion();
   if (! versiondata) {
     Serial.print("Didn't find PN53x board");
+    BT.write("Didn't find NFC Module");
     while (1); // halt
   }
-  // Got ok data, print it out!
+  
   Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
   Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
   Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
+  BT.write("Found NFC Module");
   
   // configure board to read RFID tags
-  nfc.SAMConfig();  
+  nfc.SAMConfig();   
 }
 
 void loop(void) 
 {
+  
+  int buttonState = digitalRead(buttonPin);
+  
+  if (buttonState == HIGH) 
+  {
   uint8_t success;
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
@@ -49,8 +63,7 @@ void loop(void)
   Serial.println("\r\nPlace your NDEF formatted NTAG2xx tag on the reader to update the");
   Serial.println("NDEF record and press any key to continue ...\r\n");
   // Wait for user input before proceeding
-  while (!Serial.available());
-  // a key was pressed1
+  
   while (Serial.available()) Serial.read();
 
   // 1.) Wait for an NTAG203 card.  When one is found 'uid' will be populated with
@@ -74,28 +87,10 @@ void loop(void)
     else
     {
       uint8_t data[32];
-      
-      // We probably have an NTAG2xx card (though it could be Ultralight as well)
       Serial.println("Seems to be an NTAG2xx tag (7 byte UID)");    
-      
-      // NTAG2x3 cards have 39*4 bytes of user pages (156 user bytes),
-      // starting at page 4 ... larger cards just add pages to the end of
-      // this range:
-      
-      // See: http://www.nxp.com/documents/short_data_sheet/NTAG203_SDS.pdf
-
-      // TAG Type       PAGES   USER START    USER STOP
-      // --------       -----   ----------    ---------
-      // NTAG 203       42      4             39
-      // NTAG 213       45      4             39
-      // NTAG 215       135     4             129
-      // NTAG 216       231     4             225      
-
-
-      // 3.) Check if the NDEF Capability Container (CC) bits are already set
-      // in OTP memory (page 3)
       memset(data, 0, 4);
       success = nfc.ntag2xx_ReadPage(3, data);
+      
       if (!success)
       {
         Serial.println("Unable to read the Capability Container (page 3)");
@@ -103,11 +98,7 @@ void loop(void)
       }
       else
       {
-        // If the tag has already been formatted as NDEF, byte 0 should be:
-        // Byte 0 = Magic Number (0xE1)
-        // Byte 1 = NDEF Version (Should be 0x10)
-        // Byte 2 = Data Area Size (value * 8 bytes)
-        // Byte 3 = Read/Write Access (0x00 for full read and write)
+       
         if (!((data[0] == 0xE1) && (data[1] == 0x10)))
         {
           Serial.println("This doesn't seem to be an NDEF formatted tag.");
@@ -129,12 +120,14 @@ void loop(void)
             success = nfc.ntag2xx_WritePage(i, data);
             Serial.print(".");
             if (!success)
-            {
+            {                                            
               Serial.println(" ERROR!");
+           
               return;
             }
           }
           Serial.println(" DONE!");
+          
           
           // 6.) Try to add a new NDEF URI record
           Serial.print("Writing URI as NDEF Record ... ");
@@ -142,10 +135,12 @@ void loop(void)
           if (success)
           {
             Serial.println("DONE!");
+            BT.write("DONE");
           }
           else
           {
             Serial.println("ERROR! (URI length?)");
+            BT.write("ERROR");
           }
                     
         } // CC contents NDEF record check
@@ -159,5 +154,95 @@ void loop(void)
     Serial.read();
     }
     Serial.flush();    
-  } // Start waiting for a new ISO14443A tag
+  }
+  }
+
+  // Read NFC _____________________________________________________________________________________________________________
+  
+  if (buttonState == LOW)
+  {
+    uint8_t success;
+  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+  uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+    
+  // Wait for an NTAG203 card.  When one is found 'uid' will be populated with
+  // the UID, and uidLength will indicate the size of the UUID (normally 7)
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
+  
+  if (success) {
+    // Display some basic information about the card
+    Serial.println("Found an ISO14443A card");
+    Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
+    Serial.print("  UID Value: ");
+    nfc.PrintHex(uid, uidLength);
+    Serial.println("");
+    BT.write("Find NFC Tag");
+    
+    if (uidLength == 7)
+    {
+      uint8_t data[32];
+      
+      // We probably have an NTAG2xx card (though it could be Ultralight as well)
+      Serial.println("Seems to be an NTAG2xx tag (7 byte UID)");    
+      
+      // NTAG2x3 cards have 39*4 bytes of user pages (156 user bytes),
+      // starting at page 4 ... larger cards just add pages to the end of
+      // this range:
+      
+      // See: http://www.nxp.com/documents/short_data_sheet/NTAG203_SDS.pdf
+
+      // TAG Type       PAGES   USER START    USER STOP
+      // --------       -----   ----------    ---------
+      // NTAG 203       42      4             39
+      // NTAG 213       45      4             39
+      // NTAG 215       135     4             129
+      // NTAG 216       231     4             225      
+
+      for (uint8_t i = 0; i < 42; i++) 
+      {
+        success = nfc.ntag2xx_ReadPage(i, data);
+        
+        // Display the current page number
+        Serial.print("PAGE ");
+        if (i < 10)
+        {
+          Serial.print("0");
+          Serial.print(i);
+        }
+        else
+        {
+          Serial.print(i);
+        }
+        Serial.print(": ");
+
+        // Display the results, depending on 'success'
+        if (success) 
+        {
+          // Dump the page data
+          nfc.PrintHexChar(data, 4);
+        }
+        else
+        {
+          Serial.println("Unable to read the requested page!");
+          BT.write("Can't read");
+        }
+      }      
+    }
+    else
+    {
+      Serial.println("This doesn't seem to be an NTAG203 tag (UUID length != 7 bytes)!");
+      BT.write("Can't read");
+    }
+    
+    // Wait a bit before trying again
+    Serial.println("\n\nSend a character to scan another tag!");
+    Serial.flush();
+    while (!Serial.available());
+    while (Serial.available()) {
+    Serial.read();
+    }
+    Serial.flush();    
+  }
+  }
+  
 }
